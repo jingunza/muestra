fc47270f470daf6419de4ed029521fd634f680b9
Jose Ingunza <jose.ingunza.es@gmail.com>
2022-06-22 16:59:47 +0000

    primera version con clase card y timer

--- src/app.js
+++ src/app.js
@@ -7,5 +7,42 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
 };
+let arrayNumero = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+const randomCard = () => {
+  let paloRandom = Math.floor(Math.random() * 4 + 1);
+  let numeroRandom = Math.floor(Math.random() * 13);
+  let paloAltoLugar = document.querySelector("#palo-alto");
+  let paloBajoLugar = document.querySelector(".palo-bajo");
+  let numeroLugar = document.querySelector(".numero-span");
+  if (paloRandom == 1) {
+    paloAltoLugar.innerHTML = "♦";
+    paloBajoLugar.innerHTML = "♦";
+    paloAltoLugar.className = "diamond";
+    paloBajoLugar.className = "diamond palo-bajo";
+  } else if (paloRandom == 2) {
+    paloAltoLugar.innerHTML = "♥";
+    paloBajoLugar.innerHTML = "♥";
+    paloAltoLugar.className = "heart";
+    paloBajoLugar.className = "heart palo-bajo";
+  } else if (paloRandom == 3) {
+    paloAltoLugar.innerHTML = "♠";
+    paloBajoLugar.innerHTML = "♠";
+    paloAltoLugar.className = "spade";
+    paloBajoLugar.className = "spade palo-bajo";
+  } else if (paloRandom == 4) {
+    paloAltoLugar.innerHTML = "♣";
+    paloBajoLugar.innerHTML = "♣";
+    paloAltoLugar.className = "club";
+    paloBajoLugar.className = "club palo-bajo";
+  }
+  numeroLugar.innerHTML = arrayNumero[numeroRandom];
+};
+let boton = document.querySelector("button");
+boton.addEventListener("click", function() {
+  location.reload();
+});
+setTimeout(function() {
+  location.reload();
+}, 10000);
